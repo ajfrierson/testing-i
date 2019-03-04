76f48ccedb29889304fefd4d0638e706e8f23f4d
ajfrierson <alvinfrierson@fuller.edu>
2019-03-04 13:32:38 -0800

    added armor.

---
+++ armor.js
@@ -0,0 +1,164 @@
+Mark_VI = {
+    origName: "Mark_VI",
+    name: "Mark_VI",
+    type: "armor",
+    durability: 100,
+    enhancement: 0
+  };
+  
+  CQB = {
+    origName: "CQB",
+    name: "CQB",
+    type: "armor",
+    durability: 100,
+    enhancement: 0
+  };
+  
+  EVA = {
+    origName: "EVA",
+    name: "[DUO] EVA",
+    type: "armor",
+    durability: 100,
+    enhancement: 17
+  };
+  
+  EOD = {
+    origName: "EOD",
+    name: "EOD",
+    type: "armor",
+    durability: 100,
+    enhancement: 0
+  };
+  
+  Hayabusa = {
+    origName: "Hayabusa",
+    name: "Hayabusa",
+    type: "armor",
+    durability: 100,
+    enhancement: 8
+  };
+  
+  Katana = {
+    origName: "Katana",
+    name: "Katana",
+    type: "armor",
+    durability: 100,
+    enhancement: 0
+  };
+  
+  Bungie = {
+    origName: "Bungie",
+    name: "Bungie",
+    type: "armor",
+    durability: 100,
+    enhancement: 15
+  };
+  
+  Security = {
+    origName: "Security",
+    name: "Security",
+    type: "armor",
+    durability: 100,
+    enhancement: 0
+  };
+  
+  Recon = {
+    origName: "Recon",
+    name: "Recon",
+    type: "armor",
+    durability: 100,
+    enhancement: 0
+  };
+  
+  Scout = {
+    origName: "Scout",
+    name: "Scout",
+    type: "armor",
+    durability: 100,
+    enhancement: 0
+  };
+  
+  ODST = {
+    origName: "ODST",
+    name: "ODST",
+    type: "armor",
+    durability: 100,
+    enhancement: 0
+  };
+  
+  Mark = {
+    origName: "Mark",
+    name: "Mark",
+    type: "armor",
+    durability: 100,
+    enhancement: 0
+  };
+  
+  Rogue = {
+    origName: "Rogue",
+    name: "Rogue",
+    type: "armor",
+    durability: 100,
+    enhancement: 0
+  };
+  
+  Combat = {
+    origName: "Combat",
+    name: "Combat",
+    type: "armor",
+    durability: 100,
+    enhancement: 0
+  };
+  
+  Assault = {
+    origName: "Assault",
+    name: "Assault",
+    type: "armor",
+    durability: 100,
+    enhancement: 0
+  };
+  
+  Flight = {
+    origName: "Flight",
+    name: "Flight",
+    type: "armor",
+    durability: 100,
+    enhancement: 0
+  };
+  
+  Ascetic = {
+    origName: "Ascetic",
+    name: "Ascetic",
+    type: "armor",
+    durability: 100,
+    enhancement: 3
+  };
+  
+  Commando = {
+    origName: "Commando",
+    name: "[DUO] Commando",
+    type: "armor",
+    durability: 100,
+    enhancement: 17
+  };
+  
+  module.exports = {
+    Mark_VI,
+    CQB,
+    EVA,
+    EOD,
+    Hayabusa,
+    Katana,
+    Bungie,
+    Security,
+    Recon,
+    Scout,
+    ODST,
+    Mark,
+    Rogue,
+    Combat,
+    Assault,
+    Flight,
+    Ascetic,
+    Commando
+  };
